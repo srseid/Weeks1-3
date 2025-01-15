a964f32d2a4a41f07fa5101fe9d17edc31e1c851
seid <samanthaseid11@gmail.com>
2025-01-15 10:10:32 -0500

    made clock and space invader

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    public float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, speed * Time.deltaTime * -1);
+
+
+        
+    }
+}
